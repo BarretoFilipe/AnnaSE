530785757061c04b1bb898443f211d6b1e24c4f2
Filipe Barreto <filipe.barreto@hotmail.com>
2020-03-11 23:15:46 +0000

    Add computed event and link to domains shop

--- src/App.vue
+++ src/App.vue
@@ -108,7 +108,16 @@
         <div class="card">
           <div class="card-body">
             <ul class="list-group">
-              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">{{domain}}</li>
+              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+                <div class="row">
+                  <div class="col-md">{{domain.name}}</div>
+                  <div class="col-md text-right">
+                    <a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
+                      <span class="fa fa-shopping-cart"></span>
+                    </a>
+                  </div>
+                </div>
+              </li>
             </ul>
           </div>
         </div>
@@ -128,46 +137,40 @@ export default {
       prefix: "",
       sufix: "",
       prefixes: ["Comic", "Graphic", "Book"],
-      sufixes: ["Hub", "Station", "Mart"],
-      domains: [
-        "ComicHub",
-        "ComicStation",
-        "ComicMart",
-        "GraphicHub",
-        "GraphicStation",
-        "GraphicMart",
-        "BookHub",
-        "BookStation",
-        "BookMart"
-      ]
+      sufixes: ["Hub", "Station", "Mart"]
     };
   },
   methods: {
     addPrefix(prefix) {
       this.prefixes.push(prefix);
       this.prefix = "";
-      this.generate();
     },
     deletePrefix(prefix) {
       this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
-      this.generate();
     },
     addSufix(sufix) {
       this.sufixes.push(sufix);
       this.sufix = "";
-      this.generate();
     },
     deleteSufix(sufix) {
       this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
-      this.generate();
-    },
-    generate() {
-      this.domains = [];
+    }
+  },
+  computed: {
+    domains() {
+      const domains = [];
       for (const prefix of this.prefixes) {
         for (const sufix of this.sufixes) {
-          this.domains.push(prefix + sufix);
+          const name = prefix + sufix;
+          const url = name.toLowerCase();
+          const checkout = `https://domains.google.com/m/registrar/search?searchTerm=${url}`;
+          domains.push({
+            name,
+            checkout
+          });
         }
       }
+      return domains;
     }
   }
 };
