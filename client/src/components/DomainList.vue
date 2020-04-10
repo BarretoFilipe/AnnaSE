52453e1729bdd3295184e15040033b22e7a0eadf
Filipe Barreto <36456738+BarretoFilipe@users.noreply.github.com>
2020-04-10 12:54:35 +0100

    adding and implementation vuex on app

--- client/src/components/DomainList.vue
+++ client/src/components/DomainList.vue
@@ -1,196 +1,94 @@
 <template>
-  <div>
-    <div id="main">
-      <div class="container">
-        <div class="row">
-          <div class="col-md">
-            <AppItemList
-              title="Prefix"
-              type="prefix"
-              v-bind:items="items.prefix"
-              v-on:addItem="addItem"
-              v-on:deleteItem="deleteItem"
-            ></AppItemList>
-          </div>
-          <div class="col-md">
-            <AppItemList
-              title="Suffix"
-              type="suffix"
-              v-bind:items="items.suffix"
-              v-on:addItem="addItem"
-              v-on:deleteItem="deleteItem"
-            ></AppItemList>
-          </div>
-        </div>
-        <br />
-        <div class="row">
-          <div class="col-md">
-            <h5>Domains</h5>
-          </div>
-          <div class="col-md text-right">
-            <h5>
-              <span class="badge badge-info">{{ domains.length }}</span>
-            </h5>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-body">
-            <ul class="list-group">
-              <li
-                class="list-group-item"
-                v-for="domain in domains"
-                v-bind:key="domain.name"
-              >
-                <div class="row">
-                  <div class="col-md-8">{{ domain.name }}</div>
-                  <div class="col-md-2">
-                    <div v-if="domain.available" class="text-right">
-                      <span class="badge badge-info">Available</span>
-                    </div>
-                    <div v-else class="text-right">
-                      <span class="badge badge-danger">Unavailable</span>
-                    </div>
-                  </div>
-                  <div class="col-md-2 text-right">
-                    <a
-                      class="btn btn-info"
-                      v-bind:href="domain.checkout"
-                      target="_blank"
-                    >
-                      <span class="fa fa-shopping-cart"></span>
-                    </a>
-                    &nbsp;
-                    <button class="btn btn-info" @click="openDomain(domain)">
-                      <span class="fa fa-search"></span>
-                    </button>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div>
+		<div id="main">
+			<div class="container">
+				<div class="row">
+					<div class="col-md">
+						<AppItemList
+							title="Prefix"
+							type="prefix"
+							v-bind:items="items.prefix"
+							v-on:addItem="addItem"
+							v-on:deleteItem="deleteItem"
+						></AppItemList>
+					</div>
+					<div class="col-md">
+						<AppItemList
+							title="Suffix"
+							type="suffix"
+							v-bind:items="items.suffix"
+							v-on:addItem="addItem"
+							v-on:deleteItem="deleteItem"
+						></AppItemList>
+					</div>
+				</div>
+				<br />
+				<div class="row">
+					<div class="col-md">
+						<h5>Domains</h5>
+					</div>
+					<div class="col-md text-right">
+						<h5>
+							<span class="badge badge-info">{{ domains.length }}</span>
+						</h5>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-body">
+						<ul class="list-group">
+							<li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+								<div class="row">
+									<div class="col-md-8">{{ domain.name }}</div>
+									<div class="col-md-2">
+										<div v-if="domain.available" class="text-right">
+											<span class="badge badge-info">Available</span>
+										</div>
+										<div v-else class="text-right">
+											<span class="badge badge-danger">Unavailable</span>
+										</div>
+									</div>
+									<div class="col-md-2 text-right">
+										<a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
+											<span class="fa fa-shopping-cart"></span>
+										</a>
+										&nbsp;
+										<button class="btn btn-info" @click="openDomain(domain)">
+											<span class="fa fa-search"></span>
+										</button>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import axios from "axios/dist/axios";
+import { mapState, mapActions } from "vuex";
 import AppItemList from "./AppItemList";
 
 export default {
-  name: "App",
-  components: {
-    AppItemList
-  },
-  data: function() {
-    return {
-      items: {
-        prefix: [],
-        suffix: []
-      },
-      domains: []
-    };
-  },
-  methods: {
-    addItem(item) {
-      axios({
-        url: "http://localhost:4000",
-        method: "post",
-        data: {
-          query: `
-            mutation($item: ItemInput){
-              newItem: saveItem(item: $item){
-                id
-                type
-                description
-              }
-            }
-          `,
-          variables: {
-            item
-          }
-        }
-      }).then(response => {
-        const query = response.data;
-        const newItem = query.data.newItem;
-        this.items[item.type].push(newItem);
-        this.generateDomains();
-      });
-    },
-    deleteItem(item) {
-      axios({
-        url: "http://localhost:4000",
-        method: "post",
-        data: {
-          query: `
-            mutation ($id: Int){
-              deleted: deleteItem(id: $id)
-            }
-          `,
-          variables: {
-            id: item.id
-          }
-        }
-      }).then(() => {
-        this.items[item.type].splice(this.items[item.type].indexOf(item), 1);
-        this.generateDomains();
-      });
-    },
-    getItems(type) {
-      return axios({
-        url: "http://localhost:4000",
-        method: "post",
-        data: {
-          query: `
-            query ($type: String) {
-              items: items (type: $type){
-                id
-                type
-                description
-              }
-          }
-        `,
-          variables: {
-            type
-          }
-        }
-      }).then(response => {
-        const query = response.data;
-        this.items[type] = query.data.items;
-      });
-    },
-    generateDomains() {
-      axios({
-        url: "http://localhost:4000",
-        method: "post",
-        data: {
-          query: `
-            mutation {
-              domains: generateDomains{
-                name
-                checkout
-                available
-              }
-            }
-          `
-        }
-      }).then(response => {
-        const query = response.data;
-        this.domains = query.data.domains;
-      });
-    },
-    openDomain(domain) {
-      this.$router.push({
-        path: `/domains/${domain.name}`
-      });
-    }
-  },
-  created() {
-    Promise.all([this.getItems("prefix"), this.getItems("suffix")]).then(() => {
-      this.generateDomains();
-    });
-  }
+	name: "App",
+	components: {
+		AppItemList
+	},
+	data: function() {
+		return {};
+	},
+	methods: {
+		...mapActions(["addItem", "deleteItem", "getItems", "generateDomains"]),
+		openDomain(domain) {
+			this.$router.push({
+				path: `/domains/${domain.name}`
+			});
+		}
+	},
+	computed: {
+		...mapState(["items", "domains"])
+	}
 };
 </script>
 
